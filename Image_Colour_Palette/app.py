69ad30414913ca4c500fade51cfb1fd63bb78449
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2023-12-13 10:12:37 -0500

    Update app.py

--- Image_Colour_Palette/app.py
+++ Image_Colour_Palette/app.py
@@ -1,34 +1,47 @@
 # Image Colour Palette
 
-''' The script creates a website allowing users to upload an image, then processes it to identify and display 
-the top 10 most common colors present in the image. '''
+''' The code allows the user to upload an image, and display 
+the top 10 most common colors in it. '''
 
 from flask import Flask, render_template, request
 from PIL import Image
 import io
 from collections import Counter
 
-app = Flask(__name__)  # Create a Flask web application instance named 'app'
+# Create a Flask web application instance named 'app'
+app = Flask(__name__)  
 
-@app.route('/')  # Define a route for the root URL '/'
+# Define a route for the root URL '/'
+@app.route('/')  
 def index():
-    return render_template('index.html')  # Render the 'index.html' template when accessing the root URL
 
-@app.route('/upload', methods=['POST'])  # Define a route for '/upload' that accepts POST requests
+    # Render the 'index.html' template when accessing the root URL
+    return render_template('index.html')  
+
+# Define a route for '/upload' that accepts POST requests
+@app.route('/upload', methods=['POST'])  
 def upload():
-    uploaded_file = request.files['image']  # Get the uploaded image file
+    # Get the uploaded image file
+    uploaded_file = request.files['image']  
     if uploaded_file.filename != '':
-        img = Image.open(io.BytesIO(uploaded_file.read()))  # Open the uploaded image file
-        img = img.convert('RGB')  # Convert image to RGB format
-        img.thumbnail((200, 200))  # Resize image for faster processing
+        # Open the uploaded image file
+        img = Image.open(io.BytesIO(uploaded_file.read()))  
+        # Convert image to RGB format
+        img = img.convert('RGB')  
+        # Resize image for faster processing
+        img.thumbnail((200, 200))  
 
-        # Get the most common colors
-        colors = img.getcolors(img.width * img.height)  # Count occurrences of each color
+        # Count occurrences of each color
+        colors = img.getcolors(img.width * img.height)  
         if colors:
-            sorted_colors = sorted(colors, key=lambda x: -x[0])[:10]  # Get top 10 most common colors
-            return render_template('index.html', colors=sorted_colors)  # Render 'index.html' with top colors
-    
-    return render_template('index.html')  # Render 'index.html' if no image uploaded or processed
+            # Get top 10 most common colors
+            sorted_colors = sorted(colors, key=lambda x: -x[0])[:10]  
+            # Render 'index.html' with top colors
+            return render_template('index.html', colors=sorted_colors)  
+
+    # Render 'index.html' if no image uploaded or processed
+    return render_template('index.html')  
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the Flask app in debug mode if this script is executed directly
+    # Run the Flask app in debug mode if this script is executed directly
+    app.run(debug=True)  
