ab57be8590583412e172ea0bdebcd90183b4c77c
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2023-12-13 10:31:50 -0500

    Update question_model.py

--- Quiz/question_model.py
+++ Quiz/question_model.py
@@ -1,5 +1,7 @@
 class Question:
+    # Initialize the Question object with text and answer attributes
     def __init__(self, q_text, q_answer):
-        # Initialize the Question object with text and answer attributes
-        self.text = q_text  # Store the text of the question
-        self.answer = q_answer  # Store the correct answer to the question
+        # Store the text of the question
+        self.text = q_text  
+        # Store the correct answer to the question
+        self.answer = q_answer  
