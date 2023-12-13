9c9a632e5f4ea8c6fe60c6bc25967b6c18e3eef4
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2023-12-13 10:29:51 -0500

    Update scoreboard.py

--- Pong/scoreboard.py
+++ Pong/scoreboard.py
@@ -3,24 +3,39 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("white")  # Set the text color of the scoreboard to white
-        self.penup()  # Lift the pen to avoid drawing when moving
-        self.hideturtle()  # Hide the turtle icon
-        self.l_score = 0  # Initialize the left player's score to 0
-        self.r_score = 0  # Initialize the right player's score to 0
-        self.update_scoreboard()  # Update the scoreboard to display the initial scores
+        # Set the text color of the scoreboard to white
+        self.color("white")  
+        # Lift the pen to avoid drawing when moving
+        self.penup() 
+        # Hide the turtle icon
+        self.hideturtle()  
+        # Initialize the left player's score to 0
+        self.l_score = 0  
+        # Initialize the right player's score to 0
+        self.r_score = 0  
+        # Update the scoreboard to display the initial scores
+        self.update_scoreboard()  
 
     def update_scoreboard(self):
-        self.clear()  # Clear previous score display
-        self.goto(-100, 200)  # Position the turtle to display the left player's score
-        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))  # Display the left player's score
-        self.goto(100, 200)  # Position the turtle to display the right player's score
-        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))  # Display the right player's score
+        # Clear previous score display
+        self.clear()  
+        # Position the turtle to display the left player's score
+        self.goto(-100, 200)  
+        # Display the left player's score
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))  
+        # Position the turtle to display the right player's score
+        self.goto(100, 200)  
+        # Display the right player's score
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))  
 
     def l_point(self):
-        self.l_score += 1  # Increment the left player's score by 1
-        self.update_scoreboard()  # Update the scoreboard to reflect the score change
+        # Increment the left player's score by 1
+        self.l_score += 1  
+        # Update the scoreboard to reflect the score change
+        self.update_scoreboard()  
 
     def r_point(self):
-        self.r_score += 1  # Increment the right player's score by 1
-        self.update_scoreboard()  # Update the scoreboard to reflect the score change
+        # Increment the right player's score by 1
+        self.r_score += 1  
+        # Update the scoreboard to reflect the score change
+        self.update_scoreboard()  
