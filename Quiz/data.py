2c6c071ee104c6a0eacf1d4c6e11597ee883552e
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2023-12-14 14:16:34 +0000

    Change Files

--- Quiz/data.py
+++ Quiz/data.py
@@ -1,20 +1,11 @@
-# Import the 'requests' library to handle HTTP requests.
+import requests
 
-# Parameters specifying the number and type of trivia questions.
 parameters = {
-    # Number of questions to retrieve.
-    "amount": 50, 
-    # Type of questions, in this case, multiple-choice questions with true/false answers.
-    "type": "boolean",   
+    "amount": 10,
+    "type": "boolean",
 }
 
-# Send a GET request to the specified API URL with the defined parameters.
 response = requests.get("https://opentdb.com/api.php", params=parameters)
-
-# Check for any HTTP errors in the response, raising an exception if an error occurs.
 response.raise_for_status()
-
-# Parse the response as JSON data and retrieve the question data from the 'results' field.
 data = response.json()
-# Extracted question data from the API response.
-question_data = data["results"]  
+question_data = data["results"]
