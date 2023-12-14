d004de05eab1b28f2cc03ddad7f0abedcdb1f24b
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2023-12-14 09:06:40 -0500

    Delete Quiz directory

--- Quiz/question_model.py
+++
@@ -1,7 +0,0 @@
-class Question:
-    # Initialize the Question object with text and answer attributes
-    def __init__(self, q_text, q_answer):
-        # Store the text of the question
-        self.text = q_text  
-        # Store the correct answer to the question
-        self.answer = q_answer  
